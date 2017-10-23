e481987906a3d1008ba5b4360dffdbb459af2a89
StephBruyere <stephbruyeredesigns@gmail.com>
2017-10-22 20:23:47 -0400

    Complete files, debugging

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,5 @@ CREATE TABLE burger (
 id int NOT NULL AUTO_INCREMENT,
 burger_name varchar(255) NOT NULL,
 devoured BOOLEAN DEFAULT false,
-datename int NOT NULL,
 PRIMARY KEY (id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burger (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burger (burger_name) VALUES ('Bacon Burger');
+INSERT INTO burger (burger_name) VALUES ('Kelp Burger');
