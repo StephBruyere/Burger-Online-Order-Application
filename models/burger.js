8064025fa9ae002764efcacabfb9a066a5f4577c
Steph Bruyere <sjones11486@yahoo.com>
2017-10-24 17:57:27 -0400

    bug fix

--- models/burger.js
+++ models/burger.js
@@ -1,31 +1,29 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-  table: 'burger',
-  
-  all: function(cb) {
-    orm.all("burger", function(res) {
-      cb(res);
-    });
-  },
-  create: function(id, cb) {
-    orm.create("burger", [
-      "burger_name", "devoured"
-    ], [
-      name, false
-    ], cb);
-  },
-  update: function(id, cb) {
-    var condition = "id=" + id;
-    orm.update("burger", {
-      devoured: true
-    }, condition, cb);
-  },
-   delete: function(id, cb) {
-   orm.delete(this.table, condition, function(res) {
-      cb(res);
-  },
+
+    all: function(cb) {
+        orm.all("burger", function(res) {
+            cb(res);
+        });
+    },
+    create: function(id, cb) {
+        orm.create("burger", [
+            "burger_name", "devoured"
+        ], [
+            name, false
+        ], cb);
+    },
+    update: function(id, cb) {
+        var condition = "id=" + id;
+        orm.update("burger", {
+            devoured: true
+        }, condition, cb);
+    },
+    delete: function(id, cb) {
+        orm.delete(this.table, condition, function(res) {
+            cb(res);
+        })
+    }
 }
-
 module.exports = burger;
-
